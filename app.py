c5f22762c3394f982b1f87b838d43ebfc550b19a
Sandra Rodgers <sandra.rodgers@deepgram.com>
2024-03-22 16:50:20 -0500

    basic functionality

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+
+from deepgram import DeepgramClient, SpeakOptions
+
+load_dotenv()
+
+app = Flask(__name__, static_folder="./static", static_url_path="/static")
+
+def synthesize_audio(text, model):
+    try:
+        deepgram = DeepgramClient(os.environ.get("DEEPGRAM_API_KEY"))
+        options = SpeakOptions(model=model)
+        filename = os.path.join(app.static_folder, "output.mp3")
+        deepgram.speak.v("1").save(filename, {"text":text}, options)
+        return filename
+    except Exception as e:
+        raise ValueError(f"Speech synthesis failed: {str(e)}")
+
+@app.route("/", methods=["GET"])
+def serve_index():
+    return app.send_static_file("index.html")
+
+@app.route("/api", methods=["POST"])
+def synthesize_speech():
+    try:
+        data = request.get_json()
+        text = data.get('text')
+        model = data.get('model')
+
+        if not text:
+            raise ValueError("Text is required in the request")
+
+        audio_file = synthesize_audio(text, model)
+        audio_url = f"{request.url_root}static/{os.path.basename(audio_file)}"
+        print(f"Audio URL: {audio_url}")
+
+        return jsonify({"success": True, "audioUrl": audio_url})
+
+    except ValueError as ve:
+        return jsonify({"success": False, "error": str(ve)})
+
+    except Exception as e:
+        return jsonify({"success": False, "error": "Internal server error"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,121 @@
+const PLAY_STATES = {
+  NO_AUDIO: "no_audio",
+  LOADING: "loading",
+  PLAYING: "playing",
+};
+
+let playState = PLAY_STATES.NO_AUDIO;
+let audio;
+const textArea = document.getElementById("text-input");
+const errorMessage = document.querySelector("#error-message");
+
+// Function to update the play button based on the current state
+function updatePlayButton() {
+  const playButton = document.getElementById("play-button");
+  const icon = playButton.querySelector(".button-icon");
+
+  switch (playState) {
+    case PLAY_STATES.NO_AUDIO:
+      icon.className = "button-icon fa-solid fa-play";
+      break;
+    case PLAY_STATES.LOADING:
+      icon.className = "button-icon fa-solid fa-circle-notch ";
+      break;
+    case PLAY_STATES.PLAYING:
+      icon.className = "button-icon fa-solid fa-stop";
+      break;
+    default:
+      break;
+  }
+}
+
+// Function to play audio
+function playAudio(audioUrl) {
+  if (audio) {
+    stopAudio();
+  }
+  currentAudioUrl = audioUrl + "?t=" + new Date().getTime(); // Add cache-busting query parameter
+  audio = new Audio(currentAudioUrl);
+
+  audio.play();
+
+  audio.addEventListener("ended", () => {
+    console.log("Audio finished playing");
+    stopAudio();
+  });
+}
+
+// Function to stop audio
+function stopAudio() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
+
+// Function to handle the click event on the play button
+function playButtonClick() {
+  switch (playState) {
+    case PLAY_STATES.NO_AUDIO:
+      sendData();
+      break;
+    case PLAY_STATES.PLAYING:
+      stopAudio();
+      playState = PLAY_STATES.NO_AUDIO;
+      updatePlayButton();
+      break;
+    default:
+      break;
+  }
+}
+
+textArea.addEventListener("input", () => {
+  errorMessage.innerHTML = "";
+});
+
+// Function to send data to backend
+function sendData() {
+  const modelSelect = document.getElementById("models");
+  const selectedModel = modelSelect.options[modelSelect.selectedIndex].value;
+  const textInput = document.getElementById("text-input").value;
+  if (!textInput) {
+    errorMessage.innerHTML = "ERROR: Please add text!";
+  } else {
+    playState = PLAY_STATES.LOADING;
+    updatePlayButton();
+
+    const data = {
+      model: selectedModel,
+      text: textInput,
+    };
+    fetch("http://127.0.0.1:5000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Response received from server:", data);
+        playAudio(data.audioUrl);
+        playState = PLAY_STATES.PLAYING;
+        updatePlayButton();
+      })
+      .catch((error) => {
+        console.error("There was a problem with your fetch operation:", error);
+        playState = PLAY_STATES.NO_AUDIO;
+        updatePlayButton();
+      });
+  }
+}
+
+// Event listener for the click event on the play button
+document
+  .getElementById("play-button")
+  .addEventListener("click", playButtonClick);
